7652472b239f97015ec1f59db6a8c8f638301ea9
italofvm <italofernandesvm@gmail.com>
2023-03-15 13:38:58 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,45 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false)
+                                  String dataDeNascimento){
+        return service.getAll(dataDeNascimento);
+    }
+
+    @PostMapping
+    public Aluno create(@Valid @RequestBody AlunoForm form){
+        return service.create(form);
+    }
+
+    @DeleteMapping("/alunos/{id}")
+    public Aluno delete(@PathVariable Long id){
+        return null;
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id){
+        return service.getAllAvaliacaoFisicaId(id);
+    }
+
+
+
+
+}
